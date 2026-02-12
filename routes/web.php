e7d131c690a93b99715671166e0dbf613b1c39d9
mifshs <razick.akk@mail.ru>
2026-02-12 20:20:11 +0500

    vse slomalos

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return view('welcome')->with('success', 'User created successfully!');
 });
 
+Route::get('/index', function () {
+    return view('index');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
